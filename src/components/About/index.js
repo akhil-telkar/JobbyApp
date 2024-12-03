73b224f2ee0bf38f2ab906dc50d69681fbacc3b9
akhil <akhil123402@gmail.com>
2024-12-03 01:48:34 +0000

    JobbyApp

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header'
+import './index.css'
+const About = () => (
+  <>
+    <Header />
+    <div className=' about flex-col'>
+      <p>Desighned by</p>
+      <h1>Telkar Akhil Kumar</h1>
+      <p>akhil123402@gmail.com</p>
+      <p>7702854206</p>
+    </div>
+  </>
+)
+export default About
